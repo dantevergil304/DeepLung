b7812f580caa1e4d0f9f1d33d02a13ee3b37277b
dantevergil304 <voquochung304@gmail.com>
2018-09-12 07:50:45 +0700

    fix run_training_alt.sh

--- run_training_alt.sh
+++ run_training_alt.sh
@@ -4,4 +4,4 @@ set -e
 cd detector
 maxeps=2
 f=9
-CUDA_VISIBLE_DEVICES=0 python main.py --model res18 -b 12 --resume 004.ckpt --save-dir res18/retrft96$f/ --start-epoch 5 --epochs $maxeps --config config_training$f
+CUDA_VISIBLE_DEVICES=0 python main.py --model res18 -b 12 --resume 004.ckpt --save-dir res18/retrft96$f/ --start-epoch 4 --epochs $maxeps --config config_training$f
